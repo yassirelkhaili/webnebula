3190fbc6667a71161f6b7e34d7c643f05f50be99
yassirelkhaili <elkhailiyassir@gmail.com>
2023-06-24 18:23:22 +0100

    mrpropre

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css';
 import { Metadata } from 'next';
-import { Inter } from 'next/font/google'; 
 import Header from './header'
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata : Metadata = {
   title: {
