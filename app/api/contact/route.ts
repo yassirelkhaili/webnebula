2cf005c0cdec846dc616414d6537c0d89a93a2ef
yassirelkhaili <elkhailiyassir@gmail.com>
2023-07-18 10:25:47 +0100

    data saving

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -6,8 +6,52 @@ import { randomBytes } from "crypto";
 import axios from "axios";
 import { createTransport } from "nodemailer";
 import * as z from "zod";
+import { PrismaClient } from '@prisma/client'
 
 let csrf_token: string;
+const prisma = new PrismaClient()
+const validationSchema = z.object({
+  Name: z
+    .string()
+    .nonempty("Please enter your name.")
+    .min(3, { message: "Name must be at least 3 characters." })
+    .max(70, { message: "Name must not exceed 70 characters." }),
+  Email: z
+    .string()
+    .email("Please enter a valid email address.")
+    .max(255, { message: "Email must not exceed 255 characters." }),
+  Phone: z.string().regex(/^\d{10}$/i, "Please enter a valid phone number."),
+  Organisation: z
+    .string()
+    .nonempty("Please enter your organization.")
+    .max(160, { message: "Company name must not exceed 160 characters." }),
+  Subject: z
+    .string()
+    .nonempty("Please enter a subject.")
+    .max(255, { message: "Subject must not exceed 255 characters." }),
+  Message: z
+    .string()
+    .nonempty("Please enter a message.")
+    .max(2000, { message: "Message must not exceed 2000 characters." }),
+  recaptchaToken: z.string(),
+  theme: z.string(),
+});
+type validationProps = z.infer<typeof validationSchema>
+export async function saveUserContactData(validatedData : validationProps) {
+  const {Name, Email, Phone, Organisation, Subject, Message, theme} = validatedData
+  const user = await prisma.contactdata.create({
+    data: {
+      clientName: Name,
+      clientEmail: Email,
+      clientPhone: Phone,
+      clientOrg: Organisation,
+      messageSubject: Subject,
+      messageContent: Message,
+      clientTheme: theme
+    },
+  });
+  return user
+}
 
 export async function GET(request: NextRequest) {
   const token = randomBytes(32).toString("hex");
@@ -25,7 +69,7 @@ export async function GET(request: NextRequest) {
         { status: 403 }
       );
     }
-  }
+  }z
 
   const referer = request.headers.get("Referer");
   if (
@@ -54,35 +98,8 @@ export async function POST(request: NextRequest) {
     );
   }
   const data = await request.json();
-  const { Name, Phone, ...newdata } = data;
-  const validationSchema = z.object({
-    Name: z
-      .string()
-      .nonempty("Please enter your name.")
-      .min(3, { message: "Name must be at least 3 characters." })
-      .max(70, { message: "Name must not exceed 70 characters." }),
-    Email: z
-      .string()
-      .email("Please enter a valid email address.")
-      .max(255, { message: "Email must not exceed 255 characters." }),
-    Phone: z.string().regex(/^\d{10}$/i, "Please enter a valid phone number."),
-    Organisation: z
-      .string()
-      .nonempty("Please enter your organization.")
-      .max(160, { message: "Company name must not exceed 160 characters." }),
-    Subject: z
-      .string()
-      .nonempty("Please enter a subject.")
-      .max(255, { message: "Subject must not exceed 255 characters." }),
-    Message: z
-      .string()
-      .nonempty("Please enter a message.")
-      .max(2000, { message: "Message must not exceed 2000 characters." }),
-    recaptchaToken: z.string(),
-    theme: z.string(),
-  });
   try {
-    const validatedData = validationSchema.parse(newdata);
+    validationSchema.parse(data);
   } catch (err) {
     if (err instanceof z.ZodError) {
       const errors = err.errors.map((err) => {
@@ -136,6 +153,8 @@ export async function POST(request: NextRequest) {
     });
   };
   if (success) {
+    const validatedData = validationSchema.parse(data);
+    saveUserContactData(validatedData).then((user) => console.log("userData saved", user)).catch((error) => console.log("error occured", error)).finally(() => prisma.$disconnect())
     sendMail(true);
     sendMail(false);
     return new Response(
