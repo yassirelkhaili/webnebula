20183ee09a6757406a456a8d694beb6ef9712b82
yassirelkhaili <elkhailiyassir@gmail.com>
2023-07-19 20:52:33 +0100

    almost done with table

--- app/utils/checkouthandler.tsx
+++ app/utils/checkouthandler.tsx
@@ -102,22 +102,65 @@ export default function CheckoutPage({ slug }: checkoutPageProps) {
       </div>
       <div className="container mt-8">
       <Table>
-  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableCaption>A comprehensive comparison of available packages.</TableCaption>
   <TableHeader>
     <TableRow>
-      <TableHead className="w-[100px]">Invoice</TableHead>
-      <TableHead>Status</TableHead>
-      <TableHead>Method</TableHead>
-      <TableHead className="text-right">Amount</TableHead>
+      <TableHead className="font-medium" colSpan={1}>Packages</TableHead>
+      <TableHead colSpan={1}>{generatePackageTitle("basic")}</TableHead>
+      <TableHead colSpan={1}>{generatePackageTitle("standard")}</TableHead>
+      <TableHead colSpan={1}>{generatePackageTitle("premium")}</TableHead>
     </TableRow>
   </TableHeader>
   <TableBody>
-    <TableRow>
-      <TableCell className="font-medium">INV001</TableCell>
-      <TableCell>Paid</TableCell>
-      <TableCell>Credit Card</TableCell>
-      <TableCell className="text-right">$250.00</TableCell>
-    </TableRow>
+    {generateCheckoutContent(slug).map((checkoutItem : checkoutProps | deliveryTimeProps) => {
+      return (
+      <TableRow>
+        <TableCell className="font-medium w-[25%]">{checkoutItem.columnTitle}</TableCell>
+        <TableCell className="w-[25%]">
+            {(typeof checkoutItem.isChecked === "number")
+                  ? (checkoutItem.isChecked + (checkoutItem.columnTitle === "Delivery Time" ? " days" : ""))
+              : (
+                <svg width="21" height="19" viewBox="0 0 11 9" xmlns="http://www.w3.org/2000/svg">
+                  <path className={checkoutItem.isChecked ? "fill-logo-100 dark:fill-logo-900" : "fill-slate-500 dark:fill-slate-400"} d="M3.64489 8.10164L0.158292 4.61504C-0.0511769 4.40557 -0.0511769 4.06594 0.158292 3.85645L0.916858 3.09786C1.12633 2.88837 1.46598 2.88837 1.67545 3.09786L4.02419 5.44658L9.05493 0.41586C9.2644 0.206391 9.60405 0.206391 9.81352 0.41586L10.5721 1.17445C10.7816 1.38392 10.7816 1.72355 10.5721 1.93303L4.40348 8.10166C4.19399 8.31113 3.85436 8.31113 3.64489 8.10164V8.10164Z"></path>
+                </svg>
+              )}
+            {isDeliveryTimeProps(checkoutItem) && (
+              <span className="text-slate-600 dark:text-slate-500 block">
+                {checkoutItem.hint}
+              </span>
+            )}
+          </TableCell>
+          <TableCell className="w-[25%]">
+            {(typeof checkoutItem.isChecked === "number")
+                  ? (checkoutItem.isChecked + (checkoutItem.columnTitle === "Delivery Time" ? " days" : ""))
+              : (
+                <svg width="21" height="19" viewBox="0 0 11 9" xmlns="http://www.w3.org/2000/svg">
+                  <path className={checkoutItem.isChecked ? "fill-logo-100 dark:fill-logo-900" : "fill-slate-500 dark:fill-slate-400"} d="M3.64489 8.10164L0.158292 4.61504C-0.0511769 4.40557 -0.0511769 4.06594 0.158292 3.85645L0.916858 3.09786C1.12633 2.88837 1.46598 2.88837 1.67545 3.09786L4.02419 5.44658L9.05493 0.41586C9.2644 0.206391 9.60405 0.206391 9.81352 0.41586L10.5721 1.17445C10.7816 1.38392 10.7816 1.72355 10.5721 1.93303L4.40348 8.10166C4.19399 8.31113 3.85436 8.31113 3.64489 8.10164V8.10164Z"></path>
+                </svg>
+              )}
+            {isDeliveryTimeProps(checkoutItem) && (
+              <span className="text-slate-600 dark:text-slate-500 block">
+                {checkoutItem.hint}
+              </span>
+            )}
+          </TableCell>
+          <TableCell className="w-[25%]">
+            {(typeof checkoutItem.isChecked === "number")
+                  ? (checkoutItem.isChecked + (checkoutItem.columnTitle === "Delivery Time" ? " days" : ""))
+              : (
+                <svg width="21" height="19" viewBox="0 0 11 9" xmlns="http://www.w3.org/2000/svg">
+                  <path className={checkoutItem.isChecked ? "fill-logo-100 dark:fill-logo-900" : "fill-slate-500 dark:fill-slate-400"} d="M3.64489 8.10164L0.158292 4.61504C-0.0511769 4.40557 -0.0511769 4.06594 0.158292 3.85645L0.916858 3.09786C1.12633 2.88837 1.46598 2.88837 1.67545 3.09786L4.02419 5.44658L9.05493 0.41586C9.2644 0.206391 9.60405 0.206391 9.81352 0.41586L10.5721 1.17445C10.7816 1.38392 10.7816 1.72355 10.5721 1.93303L4.40348 8.10166C4.19399 8.31113 3.85436 8.31113 3.64489 8.10164V8.10164Z"></path>
+                </svg>
+              )}
+            {isDeliveryTimeProps(checkoutItem) && (
+              <span className="text-slate-600 dark:text-slate-500 block">
+                {checkoutItem.hint}
+              </span>
+            )}
+          </TableCell>
+      </TableRow>
+      )
+    })}
   </TableBody>
 </Table>
       </div>
